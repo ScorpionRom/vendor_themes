831205f7bac993cc9c9d4f0fe6657a598c24e10d
ElDainosor <eldainosor@gmail.com>
2018-11-13 01:23:12 +0530

    Add more QS tiles style [2/3]

--- themes.mk
+++ themes.mk
@@ -1,14 +1,24 @@
+# QS tile styles
+PRODUCT_PACKAGES += \
+    QStileCircleTrim \
+    QStileDefault \
+    QStileDualToneCircle \
+    QStileOreo \
+    QStileOreoCircleTrim \
+    QStileOreoSquircleTrim \
+    QStileSquircleTrim \
+    QStileMountain \
+    QStileCircleDualTone \
+    QStileCircleGradient \
+    QStileDottedCircle \
+    QStileNinja \
+    QStilePokesign \
+    QStileWavey
+
 # Themes
 PRODUCT_PACKAGES += \
     GBoardDark \
     SettingsDark \
     SettingsIntelligenceDark \
     SystemDark
-
-# QS tile styles
-PRODUCT_PACKAGES += \
-    QStileCircleTrim \
-    QStileDefault \
-    QStileDualToneCircle \
-    QStileSquircleTrim
 
