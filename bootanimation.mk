f0cdd29c5c2adece75b99532a9242a64f383c1d7
neporood <neporood@gmail.com>
2019-10-05 01:56:20 -0500

    bootanimation: the Scorpion way
    IF it don't work for you, talk to management...

--- bootanimation.mk
+++ bootanimation.mk
@@ -1,25 +1,8 @@
 # Boot Animation
-scr_resolution := 1080x1920
-du_device := $(patsubst %f,%,$(subst du_,,$(TARGET_PRODUCT)))
+scorpion_device := $(patsubst %f,%,$(subst scorpion_,,$(TARGET_PRODUCT)))
 
-ifneq ($(filter taimen,$(du_device)),)
-scr_resolution := 1440x2880
-endif
-
-ifneq ($(filter crosshatch,$(du_device)),)
-scr_resolution := 1440x2960
-endif
-
-ifneq ($(filter marlin,$(du_device)),)
-scr_resolution := 1440x2560
-endif
-
-ifneq ($(filter bonito sargo,$(du_device)),)
-scr_resolution := 1080x2160
-endif
-
-ifneq ($(wildcard vendor/themes/bootanimation/$(scr_resolution).zip),)
+ifneq ($(wildcard vendor/themes/bootanimation/bootanimation.zip),)
 PRODUCT_COPY_FILES += \
-    vendor/themes/bootanimation/$(scr_resolution).zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip \
-    vendor/themes/bootanimation/$(scr_resolution).zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation-dark.zip
+    vendor/themes/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip \
+    vendor/themes/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation-dark.zip
 endif
