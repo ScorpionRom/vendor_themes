fec05bc21fa0a017ec4dc1bac8ec9897b34d34fe
Alex Koskovich <zvnexus@outlook.com>
2019-10-22 15:16:43 -0400

    New wallpaper for Scorpion 3.x
    - Big "Thanks" to ROGERdotT (Roger Truttmann) for the design
    
    Change-Id: Idfba552de160800e431edf731de51ac25061de47

--- bootanimation.mk
+++ bootanimation.mk
@@ -1,6 +1,10 @@
 # Boot Animation
 scorpion_device := $(patsubst %f,%,$(subst scorpion_,,$(TARGET_PRODUCT)))
 
+PRODUCT_PACKAGES += \
+    boot_theme \
+    misc_writer_system
+
 ifneq ($(wildcard vendor/themes/bootanimation/bootanimation.zip),)
 PRODUCT_COPY_FILES += \
     vendor/themes/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip \
