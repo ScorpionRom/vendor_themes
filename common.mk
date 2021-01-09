ea38a334122613dabd26e4ea4dd3707a5aacef80
neporood <neporood@gmail.com>
2021-01-08 19:23:56 -0600

    Prep for DU Themes [4/4]
    This reverts commit 24e24b6da4f296222e57e59ae0f9854b12859047.
    
    Change-Id: Iaf3d596b0ff4d9a0c4cd7871c3eb406b5b063959

--- common.mk
+++ common.mk
@@ -1,14 +1,14 @@
 # Accents
-#include vendor/themes/accents.mk
+include vendor/themes/accents.mk
 
 # Boot animation
 include vendor/themes/bootanimation.mk
 
 # Fonts
-#include vendor/themes/fonts.mk
+include vendor/themes/fonts.mk
 
 # Misc
-#include vendor/themes/misc.mk
+include vendor/themes/misc.mk
 
 # Sounds
 include vendor/themes/sounds.mk
